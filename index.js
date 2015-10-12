883a9428456c7dd5c9759bb3e79937358e33ab2e
Yunong Xiao <yjxiao@gmail.com>
2015-10-12 12:07:07 -0700

    initial checkin

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function ReactiveSocketClientProtocol() {
+
+}
+
+module.exports = ReactiveSocketClientProtocol;
+
+ReactiveSocketClientProtocol.prototype.requestResponse =
+function requestResponse(payload) {
+
+};
+
+ReactiveSocketClientProtocol.prototype.requestStream =
+function requestStream (payload) {
+
+};
+
+ReactiveSocketClientProtocol.prototype.fireAndForget =
+function fireAndForget(payload) {
+
+};
+
+ReactiveSocketClientProtocol.prototype.requestSubscription =
+function requestSubscription(payload) {
+
+};
