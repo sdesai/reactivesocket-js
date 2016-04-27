9c70f81a831ea1535ee93fc3e7b1aaadb7154c0f
Yunong Xiao <yjxiao@gmail.com>
2016-04-27 16:03:35 -0700

    add framing stream

---
+++ test/streams/framingStream.test.js
@@ -0,0 +1,224 @@
+'use strict';
+
+var _ = require('lodash');
+var assert = require('chai').assert;
+var bunyan = require('bunyan');
+
+var getRandomInt = require('../common/getRandomInt');
+
+var FramingStream = require('../../lib/streams/framingStream');
+var ParseStream = require('../../lib/streams/parseStream.js');
+var SerializeStream = require('../../lib/streams/serializeStream');
+
+var CONSTANTS = require('../../lib/protocol/constants');
+var FLAGS = CONSTANTS.FLAGS;
+var TYPES = CONSTANTS.TYPES;
+
+describe('framing stream', function () {
+    var LOG = bunyan.createLogger({
+        name: 'framing stream test',
+        level: process.env.LOG_LEVEL || bunyan.INFO,
+        serializers: bunyan.stdSerializers
+    });
+    LOG.addSerializers({
+        buffer: function (buf) {
+            return buf.toString();
+        }
+    });
+
+    var FRAME = {
+        type: TYPES.SETUP,
+        flags: FLAGS.LEASE | FLAGS.STRICT,
+        keepalive: getRandomInt(0, Math.pow(2, 32)),
+        maxLifetime: getRandomInt(0, Math.pow(2, 32)),
+        version: CONSTANTS.VERSION,
+        metadata: 'We\'re just two lost souls swimming in a fish bowl',
+        data: 'year after year'
+    };
+
+    var F_STREAM;
+    var P_STREAM;
+
+    var FRAME_LEN;
+    var COMP_FRAME;
+    var FIRST_HALF_FRAME;
+    var SECOND_HALF_FRAME;
+
+    before(function (done) {
+        var ss = new SerializeStream({
+            log: LOG,
+            metadataEncoding: 'utf-8',
+            dataEncoding: 'ascii'
+        });
+        ss.write(FRAME);
+        ss.on('data', function (buf) {
+            COMP_FRAME = buf;
+            FRAME_LEN = COMP_FRAME.length;
+            // apparently second param of slice is length, and not index.
+            FIRST_HALF_FRAME = COMP_FRAME.slice(0, FRAME_LEN / 2);
+            SECOND_HALF_FRAME = COMP_FRAME.slice(FRAME_LEN / 2);
+            done();
+        });
+    });
+
+    beforeEach(function () {
+        F_STREAM = new FramingStream({
+            log: LOG
+        });
+        P_STREAM = new ParseStream({
+            log: LOG,
+            metadataEncoding: 'utf-8',
+            dataEncoding: 'ascii'
+        });
+        F_STREAM.pipe(P_STREAM);
+    });
+
+    it('should frame a complete frame', function (done) {
+        P_STREAM.on('data', function (actualFrame) {
+            assert.isObject(actualFrame.header);
+            assert.equal(actualFrame.header.streamId, 0,
+                         'setup frame id must be 0');
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.equal(actualFrame.header.flags,
+                         FLAGS.METADATA | FRAME.flags);
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.deepEqual(actualFrame.setup, _.omit(FRAME, 'data',
+                                                       'metadata', 'flags',
+                                                       'type'));
+            assert.deepEqual(actualFrame.data, FRAME.data);
+            assert.deepEqual(actualFrame.metadata, FRAME.metadata);
+            done();
+        });
+
+        F_STREAM.write(COMP_FRAME);
+    });
+
+    it('should not frame an incomplete stream', function () {
+        P_STREAM.on('data', function () {
+            assert.fail(null, null, 'incomplete frame should not emit data');
+        });
+        F_STREAM.write(FIRST_HALF_FRAME);
+    });
+
+    it('should frame complete and incomplete frame', function (done) {
+        var count = 0;
+        P_STREAM.on('data', function (actualFrame) {
+            count++;
+
+            if (count > 1) {
+                throw new Error('should only get one frame');
+            }
+            assert.isObject(actualFrame.header);
+            assert.equal(actualFrame.header.streamId, 0,
+                         'setup frame id must be 0');
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.equal(actualFrame.header.flags,
+                         FLAGS.METADATA | FRAME.flags);
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.deepEqual(actualFrame.setup, _.omit(FRAME, 'data',
+                                                       'metadata', 'flags',
+                                                       'type'));
+            assert.deepEqual(actualFrame.data, FRAME.data);
+            assert.deepEqual(actualFrame.metadata, FRAME.metadata);
+            done();
+        });
+
+        F_STREAM.write(Buffer.concat([COMP_FRAME, FIRST_HALF_FRAME]));
+    });
+
+    it('should frame two frames from c+i, i', function (done) {
+        var count = 0;
+        P_STREAM.on('data', function (actualFrame) {
+            count++;
+
+            if (count > 2) {
+                throw new Error('should only get two frames');
+            }
+            assert.isObject(actualFrame.header);
+            assert.equal(actualFrame.header.streamId, 0,
+                         'setup frame id must be 0');
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.equal(actualFrame.header.flags,
+                         FLAGS.METADATA | FRAME.flags);
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.deepEqual(actualFrame.setup, _.omit(FRAME, 'data',
+                                                       'metadata', 'flags',
+                                                       'type'));
+            assert.deepEqual(actualFrame.data, FRAME.data);
+            assert.deepEqual(actualFrame.metadata, FRAME.metadata);
+
+            if (count === 2) {
+                done();
+            }
+        });
+
+        F_STREAM.write(Buffer.concat([COMP_FRAME, FIRST_HALF_FRAME]));
+        F_STREAM.write(SECOND_HALF_FRAME);
+    });
+
+    it('should frame two frames from c+i, i, i', function (done) {
+        var count = 0;
+        P_STREAM.on('data', function (actualFrame) {
+            count++;
+
+            if (count > 2) {
+                throw new Error('should only get two frames');
+            }
+            assert.isObject(actualFrame.header);
+            assert.equal(actualFrame.header.streamId, 0,
+                         'setup frame id must be 0');
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.equal(actualFrame.header.flags,
+                         FLAGS.METADATA | FRAME.flags);
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.deepEqual(actualFrame.setup, _.omit(FRAME, 'data',
+                                                       'metadata', 'flags',
+                                                       'type'));
+            assert.deepEqual(actualFrame.data, FRAME.data);
+            assert.deepEqual(actualFrame.metadata, FRAME.metadata);
+
+            if (count === 2) {
+                done();
+            }
+        });
+
+        F_STREAM.write(Buffer.concat([COMP_FRAME, FIRST_HALF_FRAME]));
+        F_STREAM.write(SECOND_HALF_FRAME.slice(0,
+                                               SECOND_HALF_FRAME.length / 2));
+        F_STREAM.write(SECOND_HALF_FRAME.slice(SECOND_HALF_FRAME.length / 2));
+    });
+
+    it('should frame three frames from c+i, i, i+c', function (done) {
+        var count = 0;
+        P_STREAM.on('data', function (actualFrame) {
+            count++;
+
+            if (count > 3) {
+                throw new Error('should only get two frames');
+            }
+            assert.isObject(actualFrame.header);
+            assert.equal(actualFrame.header.streamId, 0,
+                         'setup frame id must be 0');
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.equal(actualFrame.header.flags,
+                         FLAGS.METADATA | FRAME.flags);
+            assert.equal(actualFrame.header.type, CONSTANTS.TYPES.SETUP);
+            assert.deepEqual(actualFrame.setup, _.omit(FRAME, 'data',
+                                                       'metadata', 'flags',
+                                                       'type'));
+            assert.deepEqual(actualFrame.data, FRAME.data);
+            assert.deepEqual(actualFrame.metadata, FRAME.metadata);
+
+            if (count === 3) {
+                done();
+            }
+        });
+
+        F_STREAM.write(Buffer.concat([COMP_FRAME, FIRST_HALF_FRAME]));
+        F_STREAM.write(SECOND_HALF_FRAME.slice(0,
+                                               SECOND_HALF_FRAME.length / 2));
+        F_STREAM.write(Buffer.concat(
+            [SECOND_HALF_FRAME.slice(SECOND_HALF_FRAME.length / 2),
+                COMP_FRAME]));
+    });
+});
