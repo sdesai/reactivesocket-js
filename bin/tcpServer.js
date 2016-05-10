075bc001b0aad374275be90e8bf43d0333a40745
Yunong Xiao <yjxiao@gmail.com>
2016-05-10 10:57:51 -0700

    mv scripts to bin dir

--- bin/tcpServer.js
+++ bin/tcpServer.js
@@ -1,6 +1,6 @@
+#!/bin/env node
 'use strict';
 
-var fs = require('fs');
 var net = require('net');
 
 var ss = require('simple-statistics');
@@ -9,22 +9,6 @@ var reactiveSocket = require('../lib');
 
 var PORT = process.env.PORT || 1337;
 var HOST = process.env.HOST || 'localhost';
-
-// we need to send a large enough frame to ensure we exceed the default TCP
-// loopback MTU of 16384 bytes. This is to test that framing actually works.
-// Hence we read in some select works of the Bard.
-//var HAMLET = fs.readFileSync('./test/etc/hamlet.txt', 'utf8');
-//var JULIUS_CAESAR = fs.readFileSync('./test/etc/julius_caesar.txt', 'utf8');
-
-//var RES = {
-    //metadata: JULIUS_CAESAR,
-    //data: HAMLET
-//};
-
-var RES = {
-    metadata: 'hello',
-    data: 'world'
-};
 
 var COUNT = 0;
 var TCP_SERVER = net.createServer(function (con) {
@@ -38,7 +22,6 @@ var TCP_SERVER = net.createServer(function (con) {
         },
         type: 'server'
     });
-
     RS_SERVER_CON.on('request', function (stream) {
         COUNT++;
         setImmediate(function () {
--- bin/wsServer.js
+++ bin/wsServer.js
@@ -1,6 +1,5 @@
+#!/bin/env node
 'use strict';
-
-var fs = require('fs');
 
 var ss = require('simple-statistics');
 
@@ -11,22 +10,6 @@ var reactiveSocket = require('../lib');
 
 var PORT = process.env.PORT || 1337;
 var HOST = process.env.HOST || 'localhost';
-
-// we need to send a large enough frame to ensure we exceed the default TCP
-// loopback MTU of 16384 bytes. This is to test that framing actually works.
-// Hence we read in some select works of the Bard.
-var HAMLET = fs.readFileSync('./test/etc/hamlet.txt', 'utf8');
-var JULIUS_CAESAR = fs.readFileSync('./test/etc/julius_caesar.txt', 'utf8');
-
-//var RES = {
-    //metadata: JULIUS_CAESAR,
-    //data: HAMLET
-//};
-
-var RES = {
-    metadata: 'hello',
-    data: 'world'
-};
 
 var COUNT = 0;
 var WS_SERVER = new Ws.Server({port: PORT, host: HOST});
